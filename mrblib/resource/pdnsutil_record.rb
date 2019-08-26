0370663e9a9400798f2744e153cd7209559331f1
takumin <takumiiinn@gmail.com>
2019-08-26 21:52:18 +0900

    wip: working implemented

--- mrblib/resource/pdnsutil_record.rb
+++ mrblib/resource/pdnsutil_record.rb
@@ -9,7 +9,17 @@ module ::MItamae
         define_attribute :name, type: String, required: true
         define_attribute :type, type: String, required: true
         define_attribute :ttl, type: Numeric
-        define_attribute :content, type: [String, Array, Hash], required: true
+
+        # SOA
+        define_attribute :mname, type: String
+        define_attribute :rname, type: String
+        define_attribute :refresh, type: Numeric
+        define_attribute :retries, type: Numeric
+        define_attribute :expire, type: Numeric
+        define_attribute :minimum, type: Numeric
+
+        # Other
+        define_attribute :content, type: [String, Array]
 
         self.available_actions = [:present, :absent]
       end
--- mrblib/resource_executor/pdnsutil_record.rb
+++ mrblib/resource_executor/pdnsutil_record.rb
@@ -5,6 +5,24 @@ module ::MItamae
         NotExistZoneError = Class.new(StandardError)
 
         def apply
+          if    desired.exist  &&  current.exist
+            desired.each do |k, v|
+              next if k == :config_dir
+              next if k == :config_name
+
+              if current.key?(k)
+                if current[k] != v
+                  MItamae.logger.debug "k:#{k} v:#{v}"
+                end
+              end
+            end
+          elsif !desired.exist &&  current.exist
+            delete_record(desired)
+          elsif desired.exist  && !current.exist
+            create_record(desired)
+          elsif !desired.exist && !current.exist
+            # nothing...
+          end
         end
 
         private
@@ -21,41 +39,65 @@ module ::MItamae
             raise NotImplementedError
           end
 
-          MItamae.logger.debug "#{desired}"
+          unless attributes.name.nil?
+            unless attributes.name.match(/\.\z/)
+              desired.name = attributes.name
+            else
+              desired.name = attributes.zone
+            end
+          else
+            desired.name = attributes.zone
+          end
+
+          if attributes.type == 'SOA'
+            unless attributes.rname.match(/\.\z/)
+              desired.rname = "#{attributes.rname}.#{attributes.zone}"
+            end
+
+            unless attributes.mname.match(/\.\z/)
+              desired.mname = "#{attributes.mname}.#{attributes.zone}"
+            end
+          end
         end
 
         def set_current_attributes(current, action)
           @commands = ['pdnsutil']
 
-          unless attributes.config_name.empty?
+          unless desired.config_name.empty?
             @commands << '--config-name'
-            @commands << attributes.config_name
+            @commands << desired.config_name
           end
 
-          unless attributes.config_dir.empty?
+          unless desired.config_dir.empty?
             @commands << '--config-dir'
-            @commands << attributes.config_dir
+            @commands << desired.config_dir
           end
 
           zones = run_command("#{@commands.join(' ')} list-all-zones").stdout.split("\n")
 
-          unless zones.include?(attributes.zone)
-            raise NotExistZoneError, "'#{attributes.zone}' zone does not exist."
+          unless zones.include?(desired.zone)
+            raise NotExistZoneError, "'#{desired.zone}' zone does not exist."
           end
 
-          results = run_command("#{@commands.join(' ')} list-zone #{attributes.zone}").stdout.split("\n")
-
-          name = attributes.name == '.' ? attributes.zone : attributes.name + '.' + attributes.zone
-          type = attributes.type
+          results = run_command("#{@commands.join(' ')} list-zone #{desired.zone}").stdout.split("\n")
 
           results.each do |result|
             case result
-            when /\A(#{Regexp.escape(name)})\s+(\d+)\s+IN\s+(#{type})\s+(.*)\z/
-              current.name = $1
-              current.ttl  = $2
-              current.type = $3
+            when /\A(#{Regexp.escape(desired.name)})\s+(\d+)\s+IN\s+(#{desired.type})\s+(.*)\z/
+              current.exist = true
+              current.name  = $1
+              current.ttl   = $2
+              current.type  = $3
 
-              if type == 'SOA'
+              if desired.type == 'SOA'
+                content         = $4.split(' ')
+                current.mname   = content[0]
+                current.rname   = content[1]
+                current.serial  = content[2].to_i
+                current.refresh = content[3].to_i
+                current.retries = content[4].to_i
+                current.expire  = content[5].to_i
+                current.minimum = content[6].to_i
               else
                 current.content = $3
               end
@@ -65,12 +107,48 @@ module ::MItamae
               current.ttl   = nil
               current.type  = nil
 
-              if type == 'SOA'
+              if desired.type == 'SOA'
+                current.mname   = nil
+                current.rname   = nil
+                current.serial  = nil
+                current.refresh = nil
+                current.retries = nil
+                current.expire  = nil
+                current.minimum = nil
               else
                 current.content = nil
               end
             end
           end
+        end
+
+        def create_record(desired)
+          # add-record ZONE NAME TYPE [ttl] content
+
+          @commands << 'add-record'
+          @commands << "'#{desired.zone}.'"
+          @commands << "'#{desired.name}.'"
+          @commands << "#{desired.type}"
+
+          if desired.key?(:ttl) and desired.ttl.is_a?(Numeric) and desired.ttl > 0
+            @commands << "#{desired.ttl}"
+          end
+
+          if desired.type == 'SOA'
+          else
+            @commands << "#{desired.content}"
+          end
+
+          MItamae.logger.debug "create_record #{@commands.join(' ')}"
+        end
+
+        def delete_record(desired)
+          MItamae.logger.debug "delete_record #{desired}"
+        end
+
+        def replace_record(current, desired)
+          MItamae.logger.debug "#{desired}"
+          MItamae.logger.debug "#{current}"
         end
       end
     end
